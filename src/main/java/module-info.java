76fa77925f4748ff91a38da4f45fbb48a865471b
trugr33n <k.lazebny@yandex.ru>
2025-04-29 00:03:33 +0700

    Ввел быстрые фиксы для корректной работы

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.monolatte.kontur {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.monolatte.kontur.view to javafx.fxml;
-    exports com.monolatte.kontur.view;
+    exports com.monolatte.kontur;
 }
